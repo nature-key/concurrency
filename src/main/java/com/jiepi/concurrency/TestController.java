d707c3ada35226a06d9f0836aa0927b684fd192e
wangxuan <wangxuan@finogeeks.com>
2019-07-24 23:17:01 +0800

    redis

--- src/main/java/com/jiepi/concurrency/TestController.java
+++ src/main/java/com/jiepi/concurrency/TestController.java
@@ -1,10 +1,13 @@
 package com.jiepi.concurrency;
 
 
+import com.jiepi.concurrency.cache.RedisClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,9 +15,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
 
 
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @Autowired
+    private RedisClient redisClient;
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
-    public String test(){
-        return  "test";
+    public String test() {
+        return "test";
+    }
+
+
+    @RequestMapping("/key")
+    @ResponseBody
+    public String get(@RequestParam("k") String k) throws Exception {
+        return redisClient.get(k);
+    }
+
+    @RequestMapping("/set")
+    @ResponseBody
+    public String set(@RequestParam("k") String k, @RequestParam("v") String v) throws Exception {
+        redisClient.set(k, v);
+        return "success";
     }
 }
